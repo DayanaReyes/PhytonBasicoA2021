d9cc97af4742b8b815ce31735d1d5693fdfdbdeb
DayanaReyes <202002364dayanaareyesr@gmail.com>
2021-02-11 13:42:11 -0600

    a

---
+++ HT1.py
@@ -0,0 +1,21 @@
+print("Ingrese un número")
+numero=int(input(""))
+i=1
+while i<=numero:
+    print("*")
+    i=i+1
+print("")
+
+
+print("Ingrese un número")
+numerodos=int(input(""))
+contador=0
+j=1
+while j<=numerodos:
+    if numerodos%j==0:
+        contador=contador+1
+    j=j+1
+if contador==2:
+    print("El numero es primo")
+if contador!=2:
+    print("El numero no es primo")
